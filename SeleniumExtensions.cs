5531f32a11df305b294cd6d3f62f8451c5346b21
Pınar Erbil <63547373+erbilpinar@users.noreply.github.com>
2021-06-22 16:17:15 +0300

    Add project files.

---
+++ SeleniumExtensions.cs
@@ -0,0 +1,72 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RakletTest
+{
+    public static class SeleniumExtensions
+    {
+        /*Wait until the linkText appears and click on it */
+        public static void click_text(this IWebDriver driver, string linkText)
+        {
+            try
+            {
+                WebDriverWait w = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+                w.Until(ExpectedConditions.ElementExists(By.LinkText(linkText)));
+                driver.FindElement(By.LinkText(linkText)).Click();
+            }
+            catch (NoSuchElementException)
+            {
+                throw new NoSuchElementException();
+            }
+            catch (TimeoutException)
+            {
+                throw new TimeoutException();
+            }
+        }
+
+        /* Wait for the element to be clickable and click on it */
+        public static void click_element(this IWebDriver driver, IWebElement element)
+        {
+            try
+            {
+                WebDriverWait w = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+                w.Until(ExpectedConditions.ElementToBeClickable(element));
+                element.Click();
+            }
+            catch (NoSuchElementException)
+            {
+                throw new NoSuchElementException();
+            }
+            catch (TimeoutException)
+            {
+                throw new TimeoutException();
+            }
+        }
+
+        /*Go to url and wait for the className element to appear and return the element */
+        public static IWebElement GoToUrl(this IWebDriver driver, string url, string className)
+        {
+            try
+            {
+                driver.Navigate().GoToUrl(@url);
+                WebDriverWait w = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+                w.Until(ExpectedConditions.ElementExists(By.ClassName(className)));
+                return driver.FindElement(By.ClassName(className));
+            }
+            catch (NoSuchElementException)
+            {
+                throw new NoSuchElementException();
+            }
+            catch (TimeoutException)
+            {
+                throw new TimeoutException();
+            }
+
+        }
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,224 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+//using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+
+namespace RakletTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public const string HomePage = "https://hello.raklet.net/";
+        public static IWebDriver driver;
+
+        [TestInitialize]
+        public void Initiliaze()
+        {
+            driver = new ChromeDriver();
+            System.Environment.SetEnvironmentVariable("webdriver.chrome.driver", @"/home/ella/PycharmProjects");
+            ChromeOptions options = new ChromeOptions();
+            options.AddExcludedArgument("disable-popup-blocking");
+            options.AddUserProfilePreference("profile.default_content_setting_values.cookies", 2);
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void TestMain()
+        {
+            IWebElement body = driver.GoToUrl(HomePage, "ApplicationContainer");
+            IList<IWebElement> href = body.FindElements(By.TagName("a"));
+            string originalWindow = driver.CurrentWindowHandle;
+
+            for (int i = 0; i < href.Count; i++)
+            {
+                body = driver.GoToUrl(HomePage, "ApplicationContainer");
+                href = body.FindElements(By.TagName("a"));
+                string link = href[i].Text;
+                driver.click_element(href[i]);
+                if (driver.WindowHandles.Count > 1)
+                {
+                    driver.SwitchTo().Window(driver.WindowHandles[1]);
+                    driver.Close();
+                    driver.SwitchTo().Window(originalWindow);
+                }
+                else
+                {
+                    if (driver.Url == HomePage)
+                    {
+                        System.Console.WriteLine("Empty link:" + link);
+                    }
+                    else
+                    {
+                        driver.Navigate().Back();
+                    }
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TestFeatures()
+        {
+            IWebElement body = driver.GoToUrl(HomePage, "ApplicationContainer");
+            driver.click_text("Features");
+            IList<IWebElement> href = body.FindElements(By.TagName("a"));
+            string originalWindow = driver.CurrentWindowHandle;
+
+            for (int i = 0; i < href.Count; i++)
+            {
+                body = driver.FindElement(By.ClassName("ApplicationContainer"));
+                href = body.FindElements(By.TagName("a"));
+                string link = href[i].Text;
+                driver.click_element(href[i]);
+                if (driver.WindowHandles.Count > 1)
+                {
+                    driver.SwitchTo().Window(driver.WindowHandles[1]);
+                    driver.Close();
+                    driver.SwitchTo().Window(originalWindow);
+                }
+                else
+                {
+                    if (driver.Url == HomePage)
+                    {
+                        System.Console.WriteLine("Main link:" + link);
+                    }
+                    driver.Navigate().Back();
+                    if (driver.Url == HomePage)
+                    {
+                        System.Console.WriteLine("Empty link:" + link);
+                        driver.Navigate().Forward();
+                    }
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TestReferences()
+        {
+            IWebElement body = driver.GoToUrl(HomePage, "ApplicationContainer");
+            driver.click_text("References");
+            IList<IWebElement> href = body.FindElements(By.TagName("a"));
+            string originalWindow = driver.CurrentWindowHandle;
+
+            for (int i = 0; i < href.Count; i++)
+            {
+                body = driver.FindElement(By.ClassName("ApplicationContainer"));
+                href = body.FindElements(By.TagName("a"));
+                string link = href[i].Text;
+                driver.click_element(href[i]);
+                if (driver.WindowHandles.Count > 1)
+                {
+                    driver.SwitchTo().Window(driver.WindowHandles[1]);
+                    driver.Close();
+                    driver.SwitchTo().Window(originalWindow);
+                }
+                else
+                {
+                    if (driver.Url == HomePage)
+                    {
+                        System.Console.WriteLine("Main link:" + link);
+                    }
+                    driver.Navigate().Back();
+                    if (driver.Url == HomePage)
+                    {
+                        System.Console.WriteLine("Empty link:" + link);
+                        driver.Navigate().Forward();
+                    }
+                }
+            }
+        }
+
+        [TestMethod]
+        public void Test_Pricing()
+        {
+            IWebElement body = driver.GoToUrl(HomePage, "ApplicationContainer");
+            driver.click_text("Pricing");
+            IList<IWebElement> href = body.FindElements(By.TagName("a"));
+            string originalWindow = driver.CurrentWindowHandle;
+            for (int i = 0; i < href.Count; i++)
+            {
+                body = driver.FindElement(By.ClassName("ApplicationContainer"));
+                href = body.FindElements(By.TagName("a"));
+                string link = href[i].Text;
+                driver.click_element(href[i]);
+                if (driver.WindowHandles.Count > 1)
+                {
+                    driver.SwitchTo().Window(driver.WindowHandles[1]);
+                    driver.Close();
+                    driver.SwitchTo().Window(originalWindow);
+                }
+                else
+                {
+                    if (driver.Url == HomePage)
+                    {
+                        System.Console.WriteLine("Main link:" + link);
+                    }
+                    driver.Navigate().Back();
+                    if (driver.Url == HomePage)
+                    {
+                        System.Console.WriteLine("Empty link:" + link);
+                        driver.Navigate().Forward();
+                    }
+                }
+            }
+
+            String xpath = "//*[@class=\"container\"]/div[2]/div[1]/div[1]/div[4]//";
+
+            for (int i = 1; i <= 8; i++)
+            {
+                var e = driver.FindElement(By.XPath(xpath + "select/option[" + i + "]"));
+                driver.click_element(e);
+                Console.WriteLine(driver.FindElement(By.XPath(xpath + "div[2]")).Text);
+            }           
+        }
+
+        [TestMethod]
+        public void TestResources()
+        {
+            IWebElement body = driver.GoToUrl(HomePage, "ApplicationContainer");
+            driver.click_text("Resources");
+            IList<IWebElement> href = body.FindElements(By.TagName("a"));
+            string originalWindow = driver.CurrentWindowHandle;
+
+            for (int i = 0; i < href.Count; i++)
+            {
+                body = driver.FindElement(By.ClassName("ApplicationContainer"));
+                href = body.FindElements(By.TagName("a"));
+                string link = href[i].Text;
+                driver.click_element(href[i]);
+                if (driver.WindowHandles.Count > 1)
+                {
+                    driver.SwitchTo().Window(driver.WindowHandles[1]);
+                    driver.Close();
+                    driver.SwitchTo().Window(originalWindow);
+                }
+                else
+                {
+                    if (driver.Url == HomePage)
+                    {
+                        System.Console.WriteLine("Main link:" + link);
+                    }
+                    driver.Navigate().Back();
+                    if (driver.Url == HomePage)
+                    {
+                        System.Console.WriteLine("Empty link:" + link);
+                        driver.Navigate().Forward();
+                    }
+                }
+            }
+        }
+    
+    }
+}
