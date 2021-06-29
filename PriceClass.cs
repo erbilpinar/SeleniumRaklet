cee9b8ee4ff4d2efcf9e695acf09f31c07a34585
Pınar Erbil <63547373+erbilpinar@users.noreply.github.com>
2021-06-29 13:53:22 +0300

    Updated the TestLinks and made it more reliable. Also updated the error handles.

--- PriceClass.cs
+++ PriceClass.cs
@@ -8,13 +8,14 @@ namespace RakletTest
 {
     public class PriceClass
     {
-        public string Price;
-        public int Contacts;
-        public int CustomFields;
-        public string Storage;
-        public string Admins;
-        public string EmailSender;
-        public string ApiLimits;
+        public string Price { get; set; }
+        public int Contacts { get; set; }
+        public int CustomFields { get; set; }
+        public string Storage { get; set; }
+        public string Admins { get; set; }
+        public string EmailSender { get; set; }
+        public string ApiLimits { get; set; }
+        public string Bar { get; set; }
         public PriceClass()
         {
             this.Price = "";
@@ -24,64 +25,6 @@ namespace RakletTest
             this.Admins = "";
             this.EmailSender = "";
             this.ApiLimits = "";
-        }
-
-        //getters and setters
-        public string GetPrice()
-        {
-            return this.Price;
-        }
-        public int GetContacts()
-        {
-            return this.Contacts;
-        }
-        public int GetCustomFields()
-        {
-            return this.CustomFields;
-        }
-        public string GetStorage()
-        {
-            return this.Storage;
-        }
-        public string GetAdmins()
-        {
-            return this.Admins;
-        }
-        public string GetEmailSender()
-        {
-            return this.EmailSender;
-        }
-        public string GetAPILimits()
-        {
-            return this.ApiLimits;
-        }
-        public void SetPrice(string price)
-        {
-            this.Price = price;
-        }
-        public void SetContacts(int contacts)
-        {
-            this.Contacts = contacts;
-        }
-        public void SetCustomFields(int customFields)
-        {
-            this.CustomFields = customFields;
-        }
-        public void SetStorage(string storage)
-        {
-            this.Storage = storage;
-        }
-        public void SetAdmins(string admins)
-        {
-            this.Admins = admins;
-        }
-        public void SetEmailSender(string emailSender)
-        {
-            this.EmailSender = emailSender;
-        }
-        public void SetAPILimits(string ApiLimits)
-        {
-            this.ApiLimits = ApiLimits;
         }
     }
 }
