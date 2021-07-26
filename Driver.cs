fda972760c7f0ecd0290ba15f32d076f9e7abe40
Pınar Erbil <63547373+erbilpinar@users.noreply.github.com>
2021-07-26 17:17:10 +0300

    Updates to popup case studies test

--- Driver.cs
+++ Driver.cs
@@ -4,11 +4,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+
 
 namespace RakletTest
 {
-    class Driver
+    public class Driver
     {
         public ChromeOptions ChromeOptions = new ChromeOptions();
         public ChromeDriverService chromeDriverService = ChromeDriverService.CreateDefaultService();
